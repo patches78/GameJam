6c6bd143e03d2f25bf764aba696e8d59db38f156
sofiazaleskikh <sofia.zaleskikh@s4campus.ag>
2026-06-27 18:27:17 +0200

    win sound

--- desertlayer/scripts/won.gd
+++ desertlayer/scripts/won.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 @onready var label = $CanvasLayer/QuitHint
 
-#func _ready():
-	
+@onready var won_sound = $Won_Sound	
 
 func _input(_event):
+	won_sound.play()
 	if Input.is_action_just_pressed("Enter"):
 		print("Enter gedrückt!")
 		get_tree().quit()
