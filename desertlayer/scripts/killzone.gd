ab0ab3554a6e526b8fa28ce86fc396c84b232e7a
sofiazaleskikh <sofia.zaleskikh@s4campus.ag>
2026-06-27 16:41:55 +0200

    killzone sound deleted

--- desertlayer/scripts/killzone.gd
+++ desertlayer/scripts/killzone.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-@onready var d_sound = $"Died_Sound"
 # cast erzeugt Typumwandlungsfehler -> deshalb manuell überprüfen
 func _on_body_entered(body) -> void:
 	# save the level
@@ -8,6 +7,5 @@ func _on_body_entered(body) -> void:
 	
 	if (body.name == "Player"):
 		print("You died.")
-		d_sound.play()
 		await get_tree().create_timer(GlobalManager.death_timeout).timeout
 		get_tree().change_scene_to_file("res://scenes/died.tscn")
